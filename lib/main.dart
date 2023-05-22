909990bc7395a9c36d9048590f6551e086c7c936
Krzysztof Kochel <k.kochel7281@gmail.com>
2023-05-22 12:14:25 +0200

    Install firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -31,28 +32,42 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Lista Ekspresowa'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.abc_outlined),
-      ),
-      body: ListView(
-        children: [
-          Container(
-            decoration: const BoxDecoration(
-              shape: BoxShape.rectangle,
-              color: Colors.cyanAccent,
+    return StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          final user = snapshot.data;
+          if (user == null) {
+            return const Scaffold(
+              body: Center(
+                child: Text('Nie jesteś zalogowany!'),
+              ),
+            );
+          }
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Lista Ekspresowa'),
             ),
-            transform: Matrix4.rotationZ(0.05),
-            padding: const EdgeInsets.all(20),
-            margin: const EdgeInsets.all(10),
-            child: const Text('Przykład 1'),
-          )
-        ],
-      ),
-    );
+            floatingActionButton: FloatingActionButton(
+              onPressed: () {},
+              child: const Icon(Icons.abc_outlined),
+            ),
+            body: ListView(
+              children: [
+                Text('Witaj! ${user.email}'),
+                const SizedBox(height: 15),
+                Container(
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    color: Colors.cyanAccent,
+                  ),
+                  transform: Matrix4.rotationZ(0.05),
+                  padding: const EdgeInsets.all(20),
+                  margin: const EdgeInsets.all(10),
+                  child: const Text('Przykład 1'),
+                )
+              ],
+            ),
+          );
+        });
   }
 }
